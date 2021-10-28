bc9e2a50c58306c29504e44a1503166dce31c354
Abraham Chalita <a01653879@itesm.mx>
2021-10-28 11:29:15 -0500

    Se agregó función de comida en movimiento random

--- vibora.py
+++ vibora.py
@@ -35,37 +35,45 @@ def inside(head):
 
 def move():
     "Move snake forward one segment."
-    head = snake[-1].copy()
-    head.move(aim)
+    head = snake[-1].copy() # posición antes de moverse
+    head.move(aim) # posición después de movimiento
+    food.move(vector(random.choice([-10, 0, 10]), random.choice([-10, 0, 10]))) # asignación de posiciones random comida
 
+    # si se sale del tablero o se come a sí misma, se dibuja en rojo la cabeza, se actualiza y termina juego
     if not inside(head) or head in snake:
         square(head.x, head.y, 9, 'red')
         update()
         return
+    # si la comida no está dentro de límites la reposiciona dentro del tablero de nuevo en posición random
+    elif not inside(food):
+        food.x = random.randrange(-15, 15) * 10
+        food.y = random.randrange(-15, 15) * 10
 
-    snake.append(head)
+    snake.append(head) # vector que agrega al cuerpo (posición)
 
     if head == food:
         print('Snake:', len(snake))
         food.x = random.randrange(-15, 15) * 10
         food.y = random.randrange(-15, 15) * 10
     else:
-        snake.pop(0)
+        snake.pop(0) # se mueve hacia el vector posterior
 
     clear()
+     # Se dibuja la comida en coordenadas correspondientes
 
     for body in snake:
         square(body.x, body.y, 9, snakeColor)
 
     square(food.x, food.y, 9, foodColor)
     update()
-    ontimer(move, 100)
+    ontimer(move, 100) # velocidad de snake
 
 
-setup(420, 420, 370, 0)
+setup(420, 420, 370, 0) # dimensiones tablero
 hideturtle()
 tracer(False)
 listen()
+# detección de teclas presionadas
 onkey(lambda: change(10, 0), 'Right')
 onkey(lambda: change(-10, 0), 'Left')
 onkey(lambda: change(0, 10), 'Up')
